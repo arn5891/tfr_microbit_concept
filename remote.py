d73b00679c12ccdcd320c44477e3e775ad25bcd8
A. N. <118304272+arn5891@users.noreply.github.com>
2026-01-19 16:23:52 -0500

    better remote

--- remote.py
+++ remote.py
@@ -1,15 +1,38 @@
 from microbit import *
 import radio
 
-display.set_pixel(0,0,9)
+states = {}
+
+def release(obj):
+    if obj == button_a:
+        t = "a"
+    else:
+        t = "b"
+    r = False
+    if t not in states:
+        states[t] = obj.is_pressed()
+    if states[t] != obj.is_pressed() and obj.is_pressed() == False:
+        r = True
+    states[t] = obj.is_pressed()
+    return r
+    
 radio.on()
 radio.config(group=1)
 radio.config(power=1)
 radio.config(group=1)
+go = False
+stp = False
+
 while True:
-    snd = ""
-    if button_a.is_pressed():
-        snd+="A"
-    if button_b.is_pressed():
-        snd+="B"
-    radio.send(snd)
+    if release(button_a):
+        go = not go
+    if release(button_b):
+        stp = not stp    
+    if stp:
+        radio.send("0")
+        display.show('0')
+    elif go:
+        radio.send("1")
+        display.show('1')
+    else:
+        display.clear()
