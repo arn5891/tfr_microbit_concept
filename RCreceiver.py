488966c0352f35f869c8c9f3ca848f0ce005c7a9
arn5891 <118304272+arn5891@users.noreply.github.com>
2026-01-20 18:47:08 -0500

    Refactor RCreceiver.py for channel switching logic

--- RCreceiver.py
+++ RCreceiver.py
@@ -4,35 +4,23 @@ from cutebot import *
 from time import *
 
 v = 0
-ch = 1
-itvl = 90#30 to 90 ms w 30 ms intervals
+ch2 = False
+itvl = 1000#30 to 90 ms w 30 ms intervals
 switchtime = ticks_add(ticks_ms(), itvl)
 auto = False
-radio.config(group=ch)
+radio.config(group=(int(ch2)+1))
 radio.on()
 radio.config(power=1)
 while True:
-    if ch == 1:
-        m = radio.receive()
-        display.show(str(m), delay=0, wait=False)
-        if m is not None:
-            v = int(str(m))*25
-        else:
-            v = 0
-        if ticks_ms() >= switchtime:
-            switchtime = ticks_add(ticks_ms(), itvl)
-            ch = 2
-            radio.config(group=ch)
-    elif ch == 2:
-        m = radio.receive()
-        display.show(str(m), delay=0, wait=False)
-        if m is not None:
-            auto = False
-        if not auto:
-            if ticks_ms() >= switchtime:
-                switchtime = ticks_add(ticks_ms(), itvl)
-                ch = 1
-                radio.config(group=ch)
-        #else:
-        #    v = 0
+    m = ""
+    m = radio.receive()
+    display.show(str(m), delay=0, wait=False)
+    if m is not None:
+        v = int(str(m))*25
+    elif ch2 == False:
+        v = 0
+    if ticks_ms() >= switchtime:
+        switchtime = ticks_add(ticks_ms(), itvl)
+        ch2 = not ch2
+        radio.config(group=(int(ch2)+1))
     set_motors_speed(v,v)
