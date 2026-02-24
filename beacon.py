ef75a696c4cfa42193eb1c6b751edabe2c4949b6
arn5891 <118304272+arn5891@users.noreply.github.com>
2026-02-24 18:25:47 -0500

    Add length configuration and update message sending

--- beacon.py
+++ beacon.py
@@ -4,8 +4,9 @@ import radio
 radio.on()
 radio.config(group=2)
 radio.config(power=1)
+radio.config(length=100)
 
 while True:
     display.show("T")
-    msg = b'$TFR,[tfr radius in m],[ddmm.mmmm],[N/S],[dddmm.mmmm],[E/W]*'
-    radio.send_bytes(msg)
+    msg = '$TFR,[tfr radius in m],[ddmm.mmmm],[N/S],[dddmm.mmmm],[E/W]*'
+    radio.send(msg)
