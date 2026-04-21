9c181b5e8d329c5011c47b3723c6703c3c6b3a9d
arn5891 <118304272+arn5891@users.noreply.github.com>
2026-04-21 17:54:37 -0400

    Refactor radio configuration and update message format

--- beacon.py
+++ beacon.py
@@ -2,11 +2,8 @@ from microbit import *
 import radio
 
 radio.on()
-radio.config(data_rate = radio.RATE_2MBIT)
-radio.config(channel=40)
-radio.config(length=100)
-
+radio.config(data_rate = radio.RATE_2MBIT, channel = 40, length = 100, power = 7)
 while True:
     display.show("T")
-    msg = '$TFR,[radius],[ddmm.mmmm],[N/S],[dddmm.mmmm],[E/W]*'
+    msg = '$TFR,[radius],[latititude],[N/S],[longitude],[W/E]*'
     radio.send(msg)
